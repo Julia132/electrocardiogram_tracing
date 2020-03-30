594fad4bfd442703a7931a63c179fb770cbddb89
Julia132 <uka132@mail.ru>
2020-03-31 01:47:12 +0400

    created

--- plot_peak.py
+++ plot_peak.py
@@ -2,6 +2,9 @@ import heartpy as hp
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import resample
+import numpy as np
+from math import pi
+from heartpy import visualizeutils
 
 df = pd.read_csv('data.csv', header=0, sep=',', quotechar="'",)
 print(df.head(10))
@@ -46,7 +49,37 @@ wd, m = hp.process(hp.scale_data(resampled_data), sample_rate * 2)
 plt.figure(figsize=(12, 4))
 hp.plotter(wd, m)
 
-for measure in m.keys():
 
-    print('%s: %f' % (measure, m[measure]))
-print(wd)
+# for measure in m.keys():
+#
+#     print('%s: %f' % (measure, m[measure]))
+#for w in wd.keys():
+
+for sure in wd.keys():
+
+    print(sure, wd[sure])
+
+
+print('RR_indices', wd['RR_indices'][0])
+print(len(wd["RR_indices"]))
+
+plt.plot(data[180:648])
+plt.show()
+
+y = list(data[180:648])
+
+y = np.array(y)
+y = y + 100
+T = len(y) - 1
+t = np.linspace(0, len(y), num=len(y))
+y_t = y * np.sin((2*pi/T)*t)
+
+
+x = np.array(range(180, 648, 1))
+T = len(x) - 1
+t = np.linspace(0, len(x), num=len(x))
+x_t = y * np.cos((2*pi/T)*t)
+
+
+plt.plot(x_t, y_t)
+plt.show()
